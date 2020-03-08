78695e159f1c3ad62ccdd63d1b679e81790d8acc
mitsu25 <m.nakano65@gmail.com>
2020-03-08 22:01:37 +0900

    lesson_19 map word count

---
+++ lesson_19_word_count.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func countWords(text string) map[string]int {
+	words := strings.Fields(strings.ToLower(text))
+	frequencyMap := make(map[string]int, len(words))
+	for _, word := range words {
+		word = strings.Trim(word, `,.;\s`)
+		frequencyMap[word]++
+	}
+	return frequencyMap
+}
+
+func main() {
+
+	text := `
+	As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, 
+	and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. 
+	Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. 
+	Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; 
+	nothing to fear—except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man.`
+
+	frequencyMap := countWords(text)
+	for word, count := range frequencyMap {
+		if count >= 2 {
+			fmt.Printf("%v : %d\n", word, count)
+		}
+	}
+}
+
